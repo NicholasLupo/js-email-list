26156baa6d0ff0ef348b3e09b277b7660abc9ec7
NicholasLupo <nicholaslupo14@gmail.com>
2026-04-02 15:24:15 +0200

    add email generator on js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const ul = document.querySelector('.list')
-console.log(ul);
+// console.log(ul);
+
+const emailApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
+fetch(emailApi)
+    .then(resp => resp.json())
+    .then(data => {
+
+        console.log(data.response);
+
+    })
